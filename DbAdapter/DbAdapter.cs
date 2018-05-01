e48ceb8d5c5cfb47d23b3da0dcba78f746da1666
Francisco Rodriguez <frodriguez0719git config --global user.email frodriguez0719@gmail.comgit config --global user.email frodriguez0719>
2018-05-01 15:15:37 -0700

    test

---
+++ DbAdapter/DbAdapter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DbAccess.Tools;
+
+namespace DbAccess.DbAdapter
+{
+    public class DbAdapter : IDbAdapter
+    {
+        public IDbConnection Conn { get; private set; }
+        public IDbCommand Cmd { get; private set; }
+        public DbAdapter(IDbCommand command, IDbConnection conn)
+        {
+            Cmd = command;
+            Conn = conn;
+
+        }
+
+        public List<T> LoadObject<T>(string storedProcedure, IDbDataParameter[] parameters = null) where T: class
+        {
+            List<T> list = new List<T>();
+
+            using (IDbConnection conn = Conn)
+            using (IDbCommand cmd = Cmd)
+            {
+                if(conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                cmd.Connection = conn;
+                cmd.CommandTimeout = 5000;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                if(parameters != null)
+                {
+                    foreach (IDbDataParameter parameter in parameters)
+                        cmd.Parameters.Add(parameter);
+                }
+                IDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list.Add(DataMapper<T>.Instance.MaptoObject(reader));
+
+                }
+            }
+
+                return list;
+        }
+
+        public int ExecuteQuery(string storedProcedure, 
+            IDbDataParameter[] parameters, 
+            Action<IDbDataParameter[]> returnParameters = null)
+        {
+            using (IDbConnection conn = Conn)
+            using (IDbCommand cmd = Cmd)
+            {
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                cmd.Connection = conn;
+                cmd.CommandTimeout = 5000;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                foreach (IDbDataParameter parameter in parameters)
+                    cmd.Parameters.Add(parameter);
+
+                int returnValue = cmd.ExecuteNonQuery();
+                returnParameters?.Invoke(parameters);
+
+                return returnValue;
+            }
+        }
+
+        public T ExecuteScalar<T> (string storedProcedure, IDbDataParameter[] parameters)
+        {
+            using (IDbConnection conn = Conn)
+            using (IDbCommand cmd = Cmd)
+            {
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                cmd.Connection = conn;
+                cmd.CommandTimeout = 5000;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                foreach (IDbDataParameter parameter in parameters)
+                    cmd.Parameters.Add(parameter);
+
+                object obj = cmd.ExecuteScalar();
+                return (T)obj;
+
+            }
+            
+        }
+    }
+}
---
+++ Tools/DataMapper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Reflection;
+
+namespace DbAccess.Tools
+{
+    public class DataMapper<T> where T : class
+    {
+        private static readonly DataMapper<T> _instance = new DataMapper<T>();
+        PropertyInfo[] properties;
+
+        private DataMapper()
+        {
+            properties = typeof(T).GetProperties();
+        }
+        static DataMapper() { }
+        public static DataMapper<T> Instance { get { return _instance; } }
+
+        public T MaptoObject(IDataReader reader)
+        {
+            IEnumerable<string> columns = reader.GetSchemaTable()
+                .Rows.Cast<DataRow>().Select(
+                c => c["ColumnName"].ToString().ToLower()).ToList();
+
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (PropertyInfo pinfo in properties)
+            {
+                if (columns.Contains(pinfo.Name.ToLower()))
+                    {
+                    if(reader[pinfo.Name] != DBNull.Value)
+                    {
+                        if(reader[pinfo.Name].GetType()== typeof(decimal))
+                        {
+                            pinfo.SetValue(obj, reader.GetDouble(pinfo.Name));
+                        }
+                        else
+                        {
+                            pinfo.SetValue(obj, (reader.GetValue(reader.GetOrdinal(pinfo.Name)) ?? null), null);
+                        }
+                    }
+
+
+                }
+            }
+
+
+            return obj;
+        }
+
+    }
+
+    public static class DataHelper
+    {
+        //extension methods
+        public static double GetDouble(this IDataReader reader, string columnName)
+        {
+            double dbl = 0;
+            double.TryParse(reader[columnName].ToString(), out dbl);
+            return dbl;
+        }
+
+        public static double GetDouble(this IDataReader reader, int columnIndex)
+        {
+            double dbl = 0;
+            double.TryParse(reader[columnIndex].ToString(), out dbl);
+            return dbl;
+        }
+    }
+
+}
+
